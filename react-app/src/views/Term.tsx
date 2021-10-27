ab367390c71acc300d2c1a8f6a731b1736f8bf02
Erik Pearson <eapearson@lbl.gov>
2021-10-27 14:54:27 -0700

    accept uri encoded term from url

--- react-app/src/views/Term.tsx
+++ react-app/src/views/Term.tsx
@@ -15,9 +15,12 @@ export type ViewProps = RouteComponentProps<ViewParams>
 
 const TermView: React.FC<ViewProps> = (props: ViewProps) => {
     const {namespace, id, ts} = props.match.params;
+    // The term may be encoded; terms use : as a separator between
+    // the namespace and term id, and : is considered url-unsafe.
+    const term = decodeURIComponent(id);
     const ref: OntologyReference = {
         namespace,
-        term: id,
+        term,
         timestamp: (ts ? parseInt(ts) : undefined)
     };
     return <Term termRef={ref} />;
